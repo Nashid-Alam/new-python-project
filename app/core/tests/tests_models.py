fd989916c3c8e5f59a6c27c8f0cf8b344beee8e0
Nashid-Alam <nashid17@gmail.com>
2022-11-22 12:32:56 -0500

    registered the user models

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -2,6 +2,7 @@
 Tests for models.
 """
 
+from random import sample
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -18,3 +19,24 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test emails is normalized for new users"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
