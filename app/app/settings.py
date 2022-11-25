d2a2db6d684bac208351ab1819c425b9722db031
Nashid-Alam <nashid17@gmail.com>
2022-11-25 10:30:45 -0500

    added the recipe model and serializers

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'rest_framework.authtoken',
     'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
